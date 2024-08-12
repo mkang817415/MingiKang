c4a08855e8ed3211499530923ebef960bae57742
Mingi Kang <mkang2@bowdoin.edu>
2024-08-11 22:44:10 -0400

    s

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "Personal-Website",
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    },
+    extensions: ['.js', '.ts', '.vue']
+  }
 })
